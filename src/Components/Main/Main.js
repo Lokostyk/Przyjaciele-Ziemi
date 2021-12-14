ac52c9f27c68a03a70897164fe9e0b51cc7ddf29
Lokostyk <lokostyk12@gmail.com>
2021-12-14 20:06:45 +0100

    Initial commit

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,13 @@
+import "./main.scss"
+import React from 'react'
+import Navbar from "../Navbar/Navbar"
+
+function Main() {
+    return (
+        <section>
+            <Navbar />
+        </section>
+    )
+}
+
+export default Main
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import "./navbar.scss"
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <img className="logo" src="/Images/logo.jpg" />
+            <ul>
+                <li>dawd</li>
+                <li>awdaw</li>
+                <li>adwsaczx</li>
+                <li>czx</li>
+                <li>vxcvbvcb</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
