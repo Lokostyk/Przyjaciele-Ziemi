0a02ea4faa79b24fe63a2be2b69fbb4a55cd5798
Lokostyk <lokostyk12@gmail.com>
2021-12-22 23:34:09 +0100

    Small improvments

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -20,12 +20,6 @@ function News() {
     return (
         <section className="newsContainer">
             <h1>Aktualności</h1>
-            <div className="con">
-                <p className="date">Dodano: 28-02-2017</p>
-                <h2>Konkurs plastyczny Szanujmy drzewa</h2>
-                <p className="preview">Konkurs plastyczny "Szanujmy drzewa" ogłosił w ramach działalności edukacyjnej Polski Klub Ekologiczny "Przyjaciele Ziemi". Uczestnicy mieli namalować plakat, którego treść zachęcałaby do szanowania drzew oraz zachęcała do większej troski o "zielone płuca". Rozstrzygnięcie nastąpiło 10 lutego 2016 roku.</p>
-                <button className="showMore">Czytaj dalej</button>
-            </div>
             {articleData.map(item=>{
                 console.log(item)
                 return <Article key={item.id} data={item}/>
--- src/Components/News/subcomponents/Article.js
+++ src/Components/News/subcomponents/Article.js
@@ -4,7 +4,7 @@ function Article({data}){
 
     return (
         <div className='con'>
-            <p className="date">Dodano: {data.data}</p>
+            <p className="date">Dodano: {data.Data}</p>
             <h2>{data.Tytul}</h2>
             <p className="preview">{data.Wstep}</p>
             {data.Rozwiniecie !== "" && data.Rozwiniecie !== null?<button className="showMore">Czytaj dalej</button>:""}
